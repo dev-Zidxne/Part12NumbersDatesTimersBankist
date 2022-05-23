44240aa4b4a27d33d9ab28711334f85b14928f64
Zidane Innis <zidaneinnis@gmail.com>
2022-05-23 14:46:45 -0500

    The Remainder Operator Lecture

--- script.js
+++ script.js
@@ -293,7 +293,7 @@ console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
 
 /////////////////////////////////////////////////////
-*/
+
 // Math and Rounding Lecture
 
 console.log(Math.sqrt(25));
@@ -343,3 +343,32 @@ console.log((2.345).toFixed(2)); //2.35
 console.log(+(2.345).toFixed(2));
 
 // toFixed() returns a string representation of numObj that does not use exponential notation and has exactly digits digits after the decimal place. The number is rounded if necessary, and the fractional part is padded with zeros if necessary so that it has the specified length.
+////////////////////////////////////////////////
+*/
+//  The Remainder Operator
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
